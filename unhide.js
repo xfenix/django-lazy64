212296064f7425f46906a98ce2f7ef956af9a13b
Denis <ad@xfenix.ru>
2018-10-19 02:19:46 +0300

    Update unhide.js
    Placeholder for python format after js minification

--- unhide.js
+++ unhide.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    let $blocks = document.querySelectorAll('.CSS_CLASS');
+    let $blocks = document.querySelectorAll('.{css_class}');
     $blocks.forEach(function($oneBlock) {
         $oneBlock.innerHTML = window.atob(
             localStorage.getItem(
